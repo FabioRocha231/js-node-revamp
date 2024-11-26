8bade736a4902b4ee8b638dc10750ea3c776266c
FabioRocha231 <fabioharoldo1@gmail.com>
2024-11-26 09:50:45 -0300

    style: applying prettier format in index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-import { promises } from 'fs'
+import { promises } from 'fs';
 
-const filePath = './file.txt'
+const filePath = './file.txt';
 
 async function readFileAsync() {
-    try {
-        const data = await promises.readFile(filePath, 'utf-8')
-        console.log(data)
-        console.log("finished")
-    } catch (e) {
-        console.log('An error occurred...:', error)
-    }
+  try {
+    const data = await promises.readFile(filePath, 'utf-8');
+    console.log(data);
+    console.log('finished');
+  } catch (e) {
+    console.log('An error occurred...:', error);
+  }
 }
 
-readFileAsync().then(() => console.log('Success!')).catch((error) => console.log(`An error occured: ${error}`))
+readFileAsync()
+  .then(() => console.log('Success!'))
+  .catch((error) => console.log(`An error occured: ${error}`));
